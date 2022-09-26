562ed85574d801259fdec331e88d2a58558b7fc3
geno <gabrigeno@gmail.com>
2022-09-27 01:15:23 +0200

    clean codice

--- src/main.rs
+++ src/main.rs
@@ -14,87 +14,78 @@ pub struct FieldInfo {
 
 fn main() {
     println!(
-        "Benvenuto in Miners, un copia di campo minato da cui 
-puoi giocare da terminale, creato intermanente in Rust!
-Scegli la modalità di gioco:
-    1 - Principiante
-    2 - Intermedio
-    3 - Esperto
+        "Welcome to Miners, a minesweeper-like for terminal, written in Rust!
+Choose a difficulty:
+    1 - Noob
+    2 - Intermediate
+    3 - Expert
 "
     );
 
-    let mut mode = String::new();
+    let mut info: FieldInfo = init_grid(get_difficulty() as u8);
+    let are_ya_winning_son = game_loop(&mut info);
 
+    if are_ya_winning_son {
+        println!("{}", "You won! :>".green());
+    } else {
+        println!("{}", "Booom, you lost :(".red());
+    }
+}
+
+fn get_difficulty() -> i8 {
+    let mut mode = String::new();
     io::stdin().read_line(&mut mode).expect("Errore :(");
     let mode: i8 = match mode.trim().parse() {
         Ok(num) => num,
         Err(_) => -1,
     };
-
     assert!(mode > 1 || mode < 4);
-
-    let mut info: FieldInfo = init_grid(mode as u8);
-    let are_ya_winning_son = game_loop(&mut info);
-
-    if are_ya_winning_son {
-        println!("Hai vinto :>");
-    } else {
-        println!("Hai perso :(");
-    }
+    mode
 }
 
 fn print_grid(info: &FieldInfo) {
     print!("\n  ");
+    // print first row of numbers
     for i in 0..info.x {
         print!("{} ", i);
     }
     for i in 0..info.field.len() {
-        // stampa del numero
+        // print column number
         if i as i32 % info.x == 0 {
             print!("\n{} ", i as i32 / info.y);
         }
 
-        // stampa della cella
+        // print cell
         if info.visible[i] {
-            if info.field[i] == -1 {
-                print!("{} ", "o".bright_red()); // stampo la bomba
-            } else {
-                if info.field[i as usize] == 0 {
-                    print!("  "); // stampo bianco se è 0
-                } else {
-                    if info.field[i as usize] == 1 {
-                        print!("{} ", "1".blue()); // stampo il numero
-                    }
-                    if info.field[i as usize] == 2 {
-                        print!("{} ", "2".green()); // stampo il numero
-                    }
-                    if info.field[i as usize] == 3 {
-                        print!("{} ", "3".red()); // stampo il numero
-                    }
-                    if info.field[i as usize] == 4 {
-                        print!("{} ", "4".color("yellow")); // stampo il numero
-                    }
-                    if info.field[i as usize] == 5 {
-                        print!("{} ", "5".color("brown")); // stampo il numero
-                    }
-                    if info.field[i as usize] == 6 {
-                        print!("{} ", "6".color("cyan")); // stampo il numero
-                    }
-                    if info.field[i as usize] == 7 {
-                        print!("{} ", "6".color("gray")); // stampo il numero
-                    }
-                }
-            }
+            print_number(info.field[i]);
         } else if info.flagged[i] {
-            print!("{} ", "f".red()); // stampo la flag
+            print!("{} ", "f".red());
         } else {
-            print!("{}", "  ".on_truecolor(164, 164, 164)); // stampo bianco se è 0
+            print!("{}", "  ".on_truecolor(164, 164, 164)); // print gray bg
         }
     }
     println!("\n");
 }
 
-// per semplicità uso un vettore unidimensionale per accedere vec[row][col] -> vec[row*y+col]
+// print bomb or number with colors
+fn print_number(num: i32) {
+    match num {
+        -1 => print!("{} ", "o".bright_red()),
+        0 => print!("  "),
+        1 => print!("{} ", "1".blue()),
+        2 => print!("{} ", "2".green()),
+        3 => print!("{} ", "3".red()),
+        4 => print!("{} ", "4".color("yellow")),
+        5 => print!("{} ", "5".color("brown")),
+        6 => print!("{} ", "6".color("cyan")),
+        7 => print!("{} ", "7".bright_magenta()),
+        8 => print!("{} ", "8".purple()),
+        9 => print!("{} ", "8".bright_green()),
+        _ => print!("Errore in print_number"),
+    };
+}
+
+// the fn will init a modo dimentional vector, vec[row][col] -> vec[row*y+col]
 fn init_grid(mode: u8) -> FieldInfo {
     let x;
     let y;
@@ -103,16 +94,19 @@ fn init_grid(mode: u8) -> FieldInfo {
     let mut rng = rand::thread_rng();
 
     match mode {
+        // noob
         1 => {
             x = 9;
             y = 9;
             mines = 10;
         }
+        // intermediete
         2 => {
             x = 16;
             y = 16;
             mines = 40;
         }
+        // expert
         3 => {
             x = 30;
             y = 16;
@@ -121,7 +115,7 @@ fn init_grid(mode: u8) -> FieldInfo {
         _ => exit(1),
     }
 
-    // inizializzo il vettore
+    // init vectors
     let mut field: Vec<i32> = Vec::new();
     let mut flagged: Vec<bool> = Vec::new();
     let mut visible: Vec<bool> = Vec::new();
@@ -133,7 +127,7 @@ fn init_grid(mode: u8) -> FieldInfo {
         }
     }
 
-    //pizzo le mine
+    //set mines casually
     let mut c = mines;
     while c != 0 {
         let tmpx: i32 = rng.gen_range(0..x);
@@ -142,6 +136,7 @@ fn init_grid(mode: u8) -> FieldInfo {
             field[(tmpy * x + tmpx) as usize] = -1;
             c -= 1;
 
+            // add 1 around the mine selected
             if tmpy - 1 >= 0 {
                 if tmpx - 1 >= 0 && field[((tmpy - 1) * x + tmpx - 1) as usize] != -1 {
                     field[((tmpy - 1) * x + tmpx - 1) as usize] += 1;
@@ -175,14 +170,7 @@ fn init_grid(mode: u8) -> FieldInfo {
         }
     }
 
-    FieldInfo {
-        x,
-        y,
-        mines,
-        field,
-        flagged,
-        visible,
-    }
+    FieldInfo{x,y,mines,field,flagged,visible,}
 }
 
 fn game_loop(info: &mut FieldInfo) -> bool {
@@ -192,7 +180,7 @@ fn game_loop(info: &mut FieldInfo) -> bool {
 
         io::stdin().read_line(&mut user_input).expect("Errore :(");
 
-        // elabora il comando dell'utente
+        // get the user command
         let command = user_input.chars().next().unwrap();
         let y = user_input.chars().nth(1).unwrap().to_digit(10).unwrap() as i32;
         let x = user_input.chars().nth(3).unwrap().to_digit(10).unwrap() as i32;
@@ -201,26 +189,30 @@ fn game_loop(info: &mut FieldInfo) -> bool {
             info.visible[(y * info.y + x) as usize] = true;
         }
         if command == 'f' {
-            info.flagged[(y * info.y + x) as usize] = true;
-        }
-
-        // controllo per la perdità
-        for i in 0..info.mines {
-            if info.field[(y * info.y + x) as usize] == -1 && info.visible[i as usize] {
-                print_grid(&info);
-                return false; // u lost the game
+            if info.flagged[(y * info.y + x) as usize] {
+                info.flagged[(y * info.y + x) as usize] = false;
+            } else {
+                info.flagged[(y * info.y + x) as usize] = true;
             }
         }
 
-        // controllo per la vincita
+        // check loss
+        for i in 0..info.mines {
+            if info.field[(y * info.y + x) as usize] == -1 && info.visible[i as usize] {
+                print_grid(&info);
+                return false;
+            }
+        }
+
+        // check win
         let mut c = 0;
         for i in 0..info.field.len() {
             if info.visible[i] {
                 c += 1;
             }
         }
-        if c == info.visible.len() - 10 {
-            return true; // vinci se tutte le celle sono visibili tranne le mine
+        if c == info.field.len() - 10 {
+            return true;
         }
     }
 }
